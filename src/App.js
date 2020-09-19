845bf80ca75ca5fbe2421c81d853e9768ab5469d
Malik <malikmukhtar63@gmail.com>
2020-09-19 11:38:11 +0100

    managing and passing state from all components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import ComponentA from "./components/componentA";
 import ComponentB from "./components/componentB";
 import ComponentC from "./components/componentC";
 
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <ComponentA />
-      <ComponentB />
-      <ComponentC />
-    </div>
+    <CountContext.Provider
+      value={{ countState: count, countDispatch: dispatch }}
+    >
+      <div className="App">
+        Count - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
--- src/components/componentF.jsx
+++ src/components/componentF.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CountContext } from "../App";
 
 const ComponentF = () => {
-  return <div>Component F</div>;
+  const countContext = useContext(CountContext);
+  return (
+    <div>
+      Component F - {countContext.countState}
+      <button onClick={() => countContext.countDispatch("increment")}>+</button>
+      <button onClick={() => countContext.countDispatch("decrement")}>-</button>
+      <button onClick={() => countContext.countDispatch("reset")}>Reset</button>
+    </div>
+  );
 };
 
 export default ComponentF;
